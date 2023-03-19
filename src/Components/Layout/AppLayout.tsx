832014cedb6f2abb4e6864938ecdcd087129fcf9
Dennis van der Waag <dennis.van.der.waag@yoursurprise.com>
2023-03-19 21:08:41 +0100

    Removed unused components

--- src/Components/Layout/AppLayout.tsx
+++ src/Components/Layout/AppLayout.tsx
@@ -1,11 +1,8 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
 import RouteResolver from '../RouteResolver/RouteResolver';
 
 const AppLayout: React.FC = () => (
-    <Box>
-        <RouteResolver />
-    </Box>
+    <RouteResolver />
 );
 
 export default AppLayout;
